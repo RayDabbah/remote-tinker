085082b4dc7cbb5647c0423203ec3a01d0514b97
raydabbah <raydabbah@gmail.com>
2024-02-04 00:51:51 -0500

    setup the config handler

--- src/Config.php
+++ src/Config.php
@@ -5,36 +5,51 @@ namespace RemoteTinker;
 class Config
 {
 
-    private string $configFilePath;
 
     private array $data;
+
+    private $configFilePath;
 
     public function __construct()
     {
 
-        $directory = getenv("HOME") . '/.config';
+        $this->setup();
+
+    }
+
+
+    public function get($key = null)
+    {
+
+        return array_key_exists($key, $this->data) ? $this->data[$key] : $this->data;
+    }
+
+    public function set($key, $data): void
+    {
+
+
+        $this->data = [...$this->data, $key => $data];
+
+        file_put_contents($this->configFilePath, json_encode($this->data));
+
+    }
+
+    private function setup(): void
+    {
+
+        $directory = getenv("HOME") . '/.config/remote-tinker';
 
         $this->configFilePath = "$directory/remote-tinker.json";
 
         if (!is_dir($directory)) {
-            mkdir($directory);
+            mkdir($directory, recursive: true);
         }
 
         if (!is_file($this->configFilePath)) {
             touch($this->configFilePath);
         }
-    }
 
-    public static function setup()
-    {
-
-        
-    }
-
-    public static function get($key)
-    {
-
-        return;
+        $this->data = json_decode(file_get_contents($this->configFilePath), true) ?: [];
     }
 
 }
